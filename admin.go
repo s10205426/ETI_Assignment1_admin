0da64c5abc8bcecf407d9a23c6b784e94a04951f
Zhao Yan Ho <s10205426@connect.np.edu.sg>
2022-12-17 16:49:56 +0800

    added Interface for Passenger Account Creation

--- admin.go
+++ admin.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"strings"
 )
+
+type Passenger struct {
+	Username  string `json:"Username"`
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+	PhoneNo   int    `json:"PhoneNo"`
+	Email     string `json:"Email"`
+}
 
 func main() {
 outer:
 	for {
 		fmt.Println("\n=====================")
 		fmt.Println("Welcome to RideShare!\n",
-			"1. Passenger Login\n",
-			"2. Driver Login\n",
-			"3. Create Account\n",
+			"1. Create Account\n",
+			"2. Passenger Login\n",
+			"3. Driver Login\n",
 			"0. Quit")
 		fmt.Print("Enter an option: ")
 
@@ -19,9 +33,7 @@ outer:
 		fmt.Scanf("%d\n", &choice)
 
 		switch choice {
-		case 1: //passenger login
-		case 2: //driver login
-		case 3: //create account
+		case 1: //create account
 			fmt.Println("\n=====================")
 			fmt.Println("Choose User Type\n",
 				"1. Create Passenger Account\n",
@@ -34,9 +46,13 @@ outer:
 
 			if choice2 == 1 {
 				createPassenger()
-			} else {
+			} else if choice2 == 2 {
 				createDriver()
+			} else {
+				break
 			}
+		case 2: //passenger login
+		case 3: //driver login
 		case 0: //quit
 			fmt.Println("Thank you for using RideShare, Goodbye!")
 			break outer
@@ -45,7 +61,44 @@ outer:
 }
 
 func createPassenger() {
-	fmt.Println("passenger success")
+	var newPassenger Passenger
+
+	fmt.Print("Enter Username: ")
+	reader0 := bufio.NewReader(os.Stdin)
+	input0, _ := reader0.ReadString('\n')
+	newPassenger.Username = strings.TrimSpace(input0)
+
+	fmt.Print("Enter First Name: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	newPassenger.FirstName = strings.TrimSpace(input)
+
+	fmt.Print("Enter Last Name: ")
+	reader2 := bufio.NewReader(os.Stdin)
+	input2, _ := reader2.ReadString('\n')
+	newPassenger.LastName = strings.TrimSpace(input2)
+
+	fmt.Print("Enter the Phone Number: ")
+	fmt.Scanf("%d\n", &(newPassenger.PhoneNo))
+
+	fmt.Print("Enter Email: ")
+	reader3 := bufio.NewReader(os.Stdin)
+	input3, _ := reader3.ReadString('\n')
+	newPassenger.Email = strings.TrimSpace(input3)
+
+	jsonString, _ := json.Marshal(newPassenger)
+	resbody := bytes.NewBuffer(jsonString)
+
+	client := &http.Client{}
+	if req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/api/v1/passenger/"+newPassenger.Username, resbody); err == nil {
+		if res, err := client.Do(req); err == nil {
+			if res.StatusCode == 202 {
+				fmt.Println("Account for", newPassenger.Username, "created successfully")
+			} else {
+				fmt.Println("Error - Username exists!")
+			}
+		}
+	}
 }
 
 func createDriver() {
