dbc6f51627225492b4f8d4d39a84c357e03c34b7
Zhao Yan Ho <s10205426@connect.np.edu.sg>
2022-12-17 16:55:43 +0800

    updated mySQL script

--- ETI_db_Script.sql
+++ ETI_db_Script.sql
@@ -1,25 +1,23 @@
-CREATE database ETI_db;
-#DROP database ETI_db;
-USE ETI_db;
+CREATE database my_db;
+#DROP database my_db;
+USE my_db;
 CREATE TABLE Passenger
-(PassengerID INT(5) NOT NULL AUTO_INCREMENT,
+(Username varchar(10),
 FirstName varchar(20), LastName varchar(20), PhoneNo INT, Email varchar(100),
-PRIMARY KEY (PassengerID));
+PRIMARY KEY (Username));
 CREATE TABLE Driver
-(DriverID INT(5) NOT NULL AUTO_INCREMENT,
+(Username varchar(10),
 FirstName varchar(20), LastName varchar(20), PhoneNo INT, Email varchar(100), IDNo varchar(9), LicenseNo varchar(10),
-PRIMARY KEY (DriverID));
+PRIMARY KEY (Username));
 
-SELECT * from ETI_db.Passenger;
-INSERT INTO Passenger (PassengerID, FirstName, LastName, PhoneNo, Email) VALUES
-("1", "John", "Tan", 12121212, "john@gmail.com");
-INSERT INTO Passenger (PassengerID, FirstName, LastName, PhoneNo, Email) VALUES
-("2", "Tim", "Ho", 12355667, "tim@gmail.com");
+INSERT INTO Passenger (Username, FirstName, LastName, PhoneNo, Email) VALUES
+("jonn123", "John", "Tan", 12121212, "john@gmail.com");
+INSERT INTO Passenger (Username, FirstName, LastName, PhoneNo, Email) VALUES
+("timho1", "Tim", "Ho", 12355667, "tim@gmail.com");
+SELECT * from my_db.Passenger;
 
-SELECT * from ETI_db.Driver;
-INSERT INTO Driver (DriverID, FirstName, LastName, PhoneNo, Email, IDNo, LicenseNo) VALUES
-("1", "George", "Johnson", 90097007, "george@gmail.com", "T0122355K",  "SEA1234G");
-INSERT INTO Driver (DriverID, FirstName, LastName, PhoneNo, Email, IDNo, LicenseNo) VALUES
-("2", "Mike", "Miller", 78900789, "mike@gmail.com", "T0990555J",  "SBC1005F");
-
-
+INSERT INTO Driver (Username, FirstName, LastName, PhoneNo, Email, IDNo, LicenseNo) VALUES
+("george7", "George", "Johnson", 90097007, "george@gmail.com", "T0122355K",  "SEA1234G");
+INSERT INTO Driver (Username, FirstName, LastName, PhoneNo, Email, IDNo, LicenseNo) VALUES
+("mikey33", "Mike", "Miller", 78900789, "mike@gmail.com", "T0990555J",  "SBC1005F");
+SELECT * from my_db.Driver;
