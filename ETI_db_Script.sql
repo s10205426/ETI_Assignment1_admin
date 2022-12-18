cff84f5fc8cf81c2c273511fb78bd4aeebf41ead
Zhao Yan Ho <s10205426@connect.np.edu.sg>
2022-12-18 17:21:17 +0800

    updated mySQL script

--- ETI_db_Script.sql
+++ ETI_db_Script.sql
@@ -9,7 +9,22 @@ CREATE TABLE Driver
 (Username varchar(10),
 FirstName varchar(20), LastName varchar(20), PhoneNo INT, Email varchar(100), Password varchar(20), IDNo varchar(9), LicenseNo varchar(10),
 PRIMARY KEY (Username));
+CREATE TABLE CarTrip
+(ID INT(5) NOT NULL AUTO_INCREMENT,
+PassengerUsername varchar(10), DriverUsername varchar(10),
+Pickup char(6), Dropoff char(6), PickupTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, IsCompleted char(1),
+PRIMARY KEY (ID));
 
+INSERT INTO CarTrip (PassengerUsername, DriverUsername, Pickup, Dropoff, PickupTime, IsCompleted) VALUES
+("jonn123", "limmy", "334059", "455098", "2020-12-18 12:52:40", "1");
+INSERT INTO CarTrip (PassengerUsername, DriverUsername, Pickup, Dropoff, PickupTime, IsCompleted) VALUES
+("jonn123", "mikey33", "102333", "859444", "2021-12-18 12:52:40", "1");
+INSERT INTO CarTrip (PassengerUsername, DriverUsername, Pickup, Dropoff, PickupTime, IsCompleted) VALUES
+("timho1", "mikey33", "102553", "859404", "2022-12-18 12:52:40", "1");
+INSERT INTO CarTrip (PassengerUsername, DriverUsername, Pickup, Dropoff, PickupTime, IsCompleted) VALUES
+("jonn123", "george7", "107763", "733444", "2021-10-20 12:52:40", "0");
+SELECT * from my_db.CarTrip;
+ 
 INSERT INTO Passenger (Username, FirstName, LastName, PhoneNo, Email, Password) VALUES
 ("jonn123", "John", "Tan", 12121212, "john@gmail.com", "pass123");
 INSERT INTO Passenger (Username, FirstName, LastName, PhoneNo, Email, Password) VALUES
@@ -21,3 +36,5 @@ INSERT INTO Driver (Username, FirstName, LastName, PhoneNo, Email, Password, IDN
 INSERT INTO Driver (Username, FirstName, LastName, PhoneNo, Email, Password, IDNo, LicenseNo) VALUES
 ("mikey33", "Mike", "Miller", 78900789, "mike@gmail.com", "qwerty", "T0990555J",  "SBC1005F");
 SELECT * from my_db.Driver;
+
+
